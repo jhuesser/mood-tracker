73c21cd0f353582790d004ef606d62400185c158
jhuesser <github@jhuesser.ch>
2021-09-14 14:21:05 +0200

    add config

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+return (object) array(
+	'api_url' => 'ENTER_API_URL_HERE', //with ending slash
+	'app_url' => 'ENTER_APP_URL_HERE', //with ending slash
+	'app_title' => 'ENTER_APP_TITLE_HERE',
+	'telegram' => array(
+		'bot' => 'BOT_USERNAME_HERE',
+		'token' => 'BOT_TOKEN_HERE'
+	)
+	);
